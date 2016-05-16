aa7da3eec00bfaadb70e1288092f2e4f484f69b8
Daniel González Briseño <sonusbeat@hotmail.com>
2016-05-16 12:27:25 -0500

    About Fixes

--- _views/about.php
+++ _views/about.php
@@ -1,12 +1,12 @@
 <div class="about">
 	<div class="about__row clearfix">
-		<div class="about__column">
+		<div class="about__column mission">
 			<img src="/images/about/our-mission.jpg" alt="Nuestra Misión">
 			<h2>Nuestra Misión:</h2>
 			<p class="mission">Proporcionar al pasajero la asesoría y herramientas de viaje justas y necesarias para logar con ellas, cubra sus expectativas de satisfacción.</p>
 		</div> <!-- /.home__row__column -->
 
-		<div class="about__column clearfix">
+		<div class="about__column vission">
 			<img src="/images/about/our-vission.jpg" alt="Nuestra Visión">
 			<h2>Nuestra Visión:</h2>
 			<p class="vission">Hacer de nuestros servicios los de mejor calidad en la industria del viaje, provocando que nuestros clientes regresen satisfechos con la certeza que volverán con nosotros.</p>
