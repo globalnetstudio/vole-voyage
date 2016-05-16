8f685c10d516749f2ac11256cf0c5385a4a30d60
Daniel González Briseño <sonusbeat@hotmail.com>
2016-05-16 10:03:48 -0500

    Cambios al home y estilos para ie8

--- _templates/main.php
+++ _templates/main.php
@@ -17,7 +17,8 @@
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	  <!--[if lt IE 9]>
-	    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  	<link rel="stylesheet" href="/css/styles-i8.css">
+	    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>	    
 	    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	  <![endif]-->
 	</head>
