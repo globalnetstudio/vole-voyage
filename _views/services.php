f9b1cd2c10c3e14ef5fa2d442fa0dc88ddd6fb84
Daniel González Briseño <sonusbeat@hotmail.com>
2016-05-18 02:10:01 -0500

    Se terminó la página ahora sigue sacar errores

--- _views/services.php
+++ _views/services.php
@@ -1,1 +1,195 @@
-<h1>Contenido de la pagina de Servicios</h1>
+<div class="services">
+	<h2>Conoce el viejo continente “Europa”</h2>
+
+	<div class="europe">
+		<div class="row clearfix">
+			<div class="column">
+				<h3 class="hide">Paris</h3>
+				
+				<img src="/images/servicios/paris.jpg" alt="París">
+				
+				<p>Sube a la Torre Ifel y disfruta del maravilloso panorama de la bella ciudad, camina por los campos Eliseos, disfruta de su gran belleza de Ciudad Luz, pasea en barco por el Rio Rhin y disfruta de una cena romántica.</p>
+			</div> <!-- /.column -->
+		
+			<div class="column">
+				<h3 class="hide">Londres</h3>
+				
+				<img src="/images/servicios/londres.jpg" alt="Londres">
+				
+				<p>Conoce las maravillas de esta ciudad en una mezcla del virreinato y la modernidad. Se testigo del cambio de guardia en el Palacio Real así como de una visita por la ciudad sin dejar de tomar tu selfie en el “Big Ben”.</p>
+			</div> <!-- /.column -->
+		
+			<div class="column">
+				<h3 class="hide">Barcelona</h3>
+				
+				<img src="/images/servicios/barcelona.jpg" alt="Barcelona">
+				
+				<p>La gran provincia de España que deberá de ser una visita obligatoria para que disfrutes de sus tapas españolas y sus yardas de cerveza pasando por su estadio de fut-bol y si te toca suerte presencies un partido del Barca.</p>
+			</div> <!-- /.column -->
+		</div> <!-- /.row -->
+	
+		<div class="row clearfix">
+			<div class="column">
+				<h3 class="hide">Madrid</h3>
+				
+				<img src="/images/servicios/madrid.jpg" alt="Madrid">
+				
+				<p>Conoce bellos lugares de la madre patria, disfruta de un partido del Real Madrid o bien visita su majestuoso estadio.</p>
+			</div> <!-- /.column -->
+
+			<div class="column">
+				<h3 class="hide">Roma</h3>
+				
+				<img src="/images/servicios/roma.jpg" alt="Roma">
+				
+				<p>No podrás dejar de asistir al Vaticano y conocer sus salas y museos de siglos pasados y llenos de grandes historias, así como de ser testigo de una misa oficiada por su santidad.</p>
+			</div> <!-- /.column -->
+
+			<div class="column">
+				<h3 class="hide">Venecia</h3>
+				
+				<img src="/images/servicios/venecia.jpg" alt="Venecia">
+				
+				<p>Conoce este romántico y pintoresco lugar y disfruta de una cena en las Plazoletas de Venecia paseando en góndola por las aguas románticas de este inigualable destino.</p>
+			</div> <!-- /.column -->
+		</div> <!-- /.row -->
+	</div> <!-- /.row -->
+
+
+	<div class="usa clearfix">
+		<h2>Estados Unidos</h2>
+		
+		<p class="usa-description">Conoce las grandes capitales de la unión americana ya sea de turismo de shopping, de educación, de trabajo, de lo que tú quieras y disfruta de las maravillas de este hermoso país. <spam class="red-underline">Visita: San Francisco, Los Ángeles, New York, Miami, Chicago  o Houston</spam>.</p>
+		
+		<div class="row new-york clearfix">
+			<div class="left-column">
+				<img src="/images/servicios/new-york.jpg" alt="New York">
+			</div> <!-- /.left-column" -->
+			
+			<div class="right-column">
+				<h3>New York</h3>
+			
+				<p>Visita esta fabulosa ciudad de ensueño y has realidad tus expectativas recorriendo en city tour los lugares más visitados de la gran manzana. Conoce la estatua de La Libertad, llega en barco y disfruta a través del Rio Hudson las maravillas que tiene el bajo y alto Manhatan. Disfruta de las famosas obras teatrales de Broadwey, camina por el Time Square, disfruta de la modernidad de esta gran ciudad y llénate de compras al recorrer el paseo de la 5ª avenida a 3 horas de New York.</p>
+			
+				<p>Podrás conocer Boston, Washington o Filadelfia o si deseas una aventura más completa, atrévete a realizar el recorrido de ensueño que nos tiene preparado las Cataratas del Niagara.</p>
+			</div> <!-- /.right-column" -->
+		</div> <!-- /.row" -->
+
+		<div class="row disneyland clearfix">
+			<div class="left-column">
+				<h3>La Magia de Disneyland</h3>
+				
+				<p>Has realidad tus sueños y déjate llevar por la fantasía del mundo Disney ahora con sus 3 nuevos espectáculos electrizantes, al hospedarte en Diseyland Resort y celebrar juntos 60 años.Visita los cuatro parques temáticos de Disney asi como los estudios Universal y Epcot Center y dale vida a ese niño que llevas dentro. Has de Disney la tierra soñada que va más allá de los límites de las películas cinematográficas que lo han llevado a la fama.</p>
+				
+				<p>Has de tu visita a Disney  la gran aventura, donde las familias puedan sumergirse en un mundo de fantasías hechas realidad, en donde juntos emprendamos un viaje de locura, te garantizamos que disfrutaran tanto niños como adultos la aventura de Disneyland.</p>
+			</div> <!-- /.left-column" -->
+			
+			<div class="right-column">
+				<img src="/images/servicios/disneyland.jpg" alt="Disneyland">
+			</div> <!-- /.right-column -->
+		</div> <!-- /.row" -->
+
+		<div class="row las-vegas clearfix">
+			<div class="left-column">
+				<img src="/images/servicios/las-vegas.jpg" alt="Las Vegas">
+			</div> <!-- /.left-column -->
+			
+			<div class="right-column">
+				<h3>Las Vegas</h3>
+				
+				<p>Llegar a Las Vegas es encontrase con una “Ciudad Vibrante” en donde las luces y el bullicio son parte de un escenario con espectaculares hoteles y centros comerciales de impresionante arquitectura, luces y animación, todos ellos con una temática diferente que recrean ciudades y lugares importantes del mundo y algunos aspectos de la historia humana, donde las apuestas y el entretenimiento son infinitos.</p>
+
+				<ul class="las-vegas-list">
+					<li>Lo que pasa en Las Vegas, se queda en Las vegas…</li>
+					<li>Las Vegas es GRANDE..</li>
+					<li>Grandes compras, apuestas y espectaculos...</li>
+				</ul>
+			</div> <!-- /.right-column -->
+		</div> <!-- /.row" -->
+
+	</div> <!-- /.usa -->
+
+	<div class="tours-latin-america">
+		<h2>Tours y Latinoamerica</h2>
+		
+		<div class="row cruise clearfix">
+			<div class="left-column">
+				<img src="/images/servicios/crucero.jpg" alt="Crucero">
+			</div> <!-- /.left-column -->
+			
+			<div class="right-column">
+				<h3>Viaja en un crucero desde Monterrey</h3>
+				
+				<p>Experimenta un viaje en altamar, tenemos cruceros saliendo de Galveston Tx trasládate via terrestre desde Monterrey, llega a San Antonio realiza el shopping obligado y al día siguiente viaja a Galveston, sin dejar de conocer los grandes centros comerciales de Houston.</p>
+				
+				<p>Experimenta la realidad fantástica de la NASA a través de sus grandes salas de exposiciones, duerme cerca del embarcadero y deja tu coche totalmente asegurado en el estacionamiento de la naviera y toma el Shuttle que en minutos te deja a las puertas de tu barco para dar inicio a la aventura por el caribe.</p>
+			</div> <!-- /.right-column -->
+		</div> <!-- /.row -->
+		
+		<div class="row panama-cuba clearfix">
+			<div class="left-column">
+				<h3 class="hide">Panamá</h3>
+				
+				<img src="/images/servicios/panama.jpg" alt="panama">
+				
+				<p>Descubre las maravillas de este hermosos destino, conoce el espectacular canal de Panamá, recorre sus 4 salas de exhibición y posteriormente observa el paso de las grandes buques por las esclusas del canal, en esta obra del siglo realizada por el hombre.</p>
+
+				<p>Transporta tu imaginación paseando por el casco viejo de la tradicional Panamá, así como sus barrios modernos de grandes rascacielos sus importantes avenidas y monumentos de la ciudad y todo por un excelente precio. Pregúntanos y descubrirás que Panamá es mucho más de lo que te imaginas.</p>
+			</div> <!-- /.left-column -->
+			
+			<div class="right-column">
+				<h3 class="hide">Cuba</h3>
+				
+				<img src="/images/servicios/cuba.jpg" alt="Cuba">
+				
+				<p>La isla grande del caribe, es historia, cultura, tradiciones, gastronomía de excelencia, playas paradisiacas y la magia de ritmos contagiosos y tradiciones que cautivan al visitante.</p>
+				
+				<p>Disfruta descansando en sus hermosas playas de blanca arena y aguas azul turquesa en un all inclusive y transpórtate a su pasado visitando la Habana y a la vez disfrutando el maravilloso presente de su histórica capital Cubana.</p>
+			</div> <!-- /.right-column -->
+		</div> <!-- /.row -->
+	</div><!-- /.tours-latin-america -->
+
+	<div class="row sports clearfix">
+		<h2>Entradas para eventos deportivos y artísticos en Estados Unidos y todo el mundo.</h2>
+		
+		<div class="left-column">
+			<ul>
+				<li>Entradas para partidos de Futbol Americano.</li>
+				<li>Todos los partidos de la Liga Europea.</li>
+				<li>Grandes Ligas</li>
+				<li>Basquetbol</li>
+				<li>Teatro en las Vegas- Broadway N.Y.C, Londres,
+					<br><span>Opera en Paris, Milán y Verona</span>.
+				</li>
+				<li>Carreras Nascar y Formula 1 Grand Premios INDY 500,
+					<br><span>Daytona 500</span></li>
+				<li>Shows en las Vegas Cirque Du Soleil, Mystere, “O”,
+					<br><span>Ka, Love, Zumanty</span></li>
+				<li>Conciertos</li>
+			</ul>
+		</div> <!-- /.left-column -->
+
+		<div class="right-column">
+			<img src="/images/servicios/baseball.jpg" alt="Baseball">
+		</div> <!-- /.right-column -->
+	</div> <!-- /.row -->
+
+	<div class="row education clearfix">
+		<h2>Turismo Educativo</h2>
+
+		<div class="left-column">
+			<p class="description">Contamos con una serie de beneficios para los estudiantes que desean conocer el país entero, tales como:</p>
+			
+			<ul>
+			  <li>Intercambios de universidades con Estados Unidos,
+			  	<br><span>Canada y Europa.</span></li>
+			  <li>Curos de idiomas en universidades fuera de México.</li>
+			  <li>Intercambios con familias de Canada, Estados Unidos y Europa.</li>
+			</ul>
+		</div> <!-- /.left-column -->
+		
+		<div class="right-column">
+			<img src="/images/servicios/students.jpg" alt="Students">
+		</div> <!-- /.right-column -->
+	</div> <!-- /.row -->
+</div> <!-- /.services -->
